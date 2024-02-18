c005540746eb93bff90785b5a9d9eac5a25b3e6a
james <jamesmichaeldavies@outlook.com>
2024-02-18 00:04:00 +0000

    up

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PersonIcon from "@mui/icons-material/Person";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import { IconButton } from "@mui/material";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between w-full border-b border-gray-100 items-center">
+      <div className="pl-10">
+        <IconButton>
+          <PersonIcon fontSize="large" />
+        </IconButton>
+      </div>
+      <img
+        className="object-contain h-10"
+        src="https://seeklogo.com/images/T/tinder-logo-FAAE852EC0-seeklogo.com.png"
+        alt="tinder logo"
+      />
+      <div className="pr-10">
+        <IconButton>
+          <QuestionAnswerIcon fontSize="large" />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
